ac7111cec5a706747535c6d1f16c558e284a16c1
yana shick <shickya@gmail.com>
2024-04-19 04:18:06 +0300

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pizza-Company',
   plugins: [react()],
   // if i make this: its crashes app if ANY mistake
   // plugins: [react(), eslint()],
